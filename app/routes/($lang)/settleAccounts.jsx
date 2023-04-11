1f3ceab93ed98a23127cd0d84bee15c39bb4f023
enxiu <hd1783260996@163.com>
2023-04-11 11:16:51 +0800

    删除引用文件

--- app/routes/($lang)/settleAccounts.jsx
+++ app/routes/($lang)/settleAccounts.jsx
@@ -2,7 +2,6 @@
 import { useRef, useMemo, useEffect, useState } from 'react';
 import { Money } from '@shopify/hydrogen';
 import { Text } from '~/components';
-import addressList from '~/components/address';
 import fetch from '~/fetch/axios';
 
 export default function settleAccounts() {
@@ -76,6 +75,828 @@ export function ProductBox({ product }) {
 
 
 export function Information({ product }) {
+  let addressList = [
+    {
+      name: '（المحافظة）إختر',
+      value: '',
+      children: [
+        {
+          name: '（المدينة）إختر',
+          value: '',
+        },
+      ],
+    },
+    {
+      name: 'الباحة',
+      value: 'Al Baha Province',
+      children: [
+        {
+          name: '（المدينة）إختر',
+          value: '',
+        },
+        {
+          name: 'أدهم',
+          value: 'Adham',
+        },
+        {
+          name: 'العقيق',
+          value: 'Al Aqiq',
+        },
+        {
+          name: 'اللأطاولة',
+          value: 'Atawleh',
+        },
+        {
+          name: 'الباحة',
+          value: 'Al baha',
+        },
+        {
+          name: 'بلجرشي',
+          value: 'Baljurashi',
+        },
+        {
+          name: 'قلوة',
+          value: 'Qilwah',
+        },
+        {
+          name: 'المندق',
+          value: 'Al Mandaq',
+        },
+        {
+          name: 'مخواه',
+          value: 'Almakhwah',
+        },
+        {
+          name: 'المظيلف',
+          value: 'Almuzaylif',
+        },
+      ],
+    },
+    {
+      name: 'المدينة المنورة',
+      value: 'Madinah Province',
+      children: [
+        {
+          name: '（المدينة）إختر',
+          value: '',
+        },
+        {
+          name: 'بدر',
+          value: 'Badr',
+        },
+        {
+          name: 'الحناكية',
+          value: 'Al Henakiyah',
+        },
+        {
+          name: 'خيبر',
+          value: 'Khaybar',
+        },
+        {
+          name: 'المدينة',
+          value: 'Madinah',
+        },
+        {
+          name: 'مهد الدهب',
+          value: 'Mahd adh Dhahab',
+        },
+        {
+          name: 'العلا',
+          value: 'Al Ula',
+        },
+        {
+          name: 'ينبع',
+          value: 'Yanbu',
+        },
+      ],
+    },
+    {
+      name: 'الرياض',
+      value: 'Riyadh Province',
+      children: [
+        {
+          name: '（المدينة）إختر',
+          value: '',
+        },
+        {
+          name: 'عفيف',
+          value: 'Afif',
+        },
+        {
+          name: 'الأفلاج',
+          value: 'AlAflaj',
+        },
+        {
+          name: 'الغاط',
+          value: 'Al Ghat',
+        },
+        {
+          name: 'المزاحمية',
+          value: 'Al Muzahimiyah',
+        },
+        {
+          name: 'الأرطاوية',
+          value: 'Al Artawiyah',
+        },
+        {
+          name: 'الدوادمي',
+          value: 'Dawadmi',
+        },
+        {
+          name: 'الدرعية',
+          value: 'Ad Diriyah',
+        },
+        {
+          name: 'ضرما',
+          value: 'Dhurma',
+        },
+        {
+          name: 'الدلم',
+          value: 'Ad Dilam',
+        },
+        {
+          name: 'الحريق',
+          value: 'Al Hariq',
+        },
+        {
+          name: 'حوطة بني تميم',
+          value: 'Howtat Bani Tamim',
+        },
+        {
+          name: 'حريملاء',
+          value: 'Huraymila',
+        },
+        {
+          name: 'حوطة سدير',
+          value: 'Hautat Sudair',
+        },
+        {
+          name: 'جلاجل',
+          value: 'Jalajil',
+        },
+        {
+          name: 'الخرج',
+          value: 'Kharj',
+        },
+        {
+          name: 'المجمعة',
+          value: 'Al Majmaah',
+        },
+        {
+          name: 'مرات',
+          value: 'Marat',
+        },
+        {
+          name: 'العيينة',
+          value: 'Al Uyaynah',
+        },
+        {
+          name: 'القصب',
+          value: 'Al Qasab',
+        },
+        {
+          name: 'القويعية',
+          value: 'Al Quwaiiyah',
+        },
+        {
+          name: 'رماح',
+          value: 'Rumah',
+        },
+        {
+          name: 'الرياض',
+          value: 'Riyadh',
+        },
+        {
+          name: 'روضة سدير',
+          value: 'Raudat Sudair',
+        },
+        {
+          name: 'ساجر',
+          value: 'Sajir',
+        },
+        {
+          name: 'صلبوخ',
+          value: 'Salbukh',
+        },
+        {
+          name: 'شقراء',
+          value: 'Shaqra',
+        },
+        {
+          name: 'السليل',
+          value: 'As Sulayyil',
+        },
+        {
+          name: 'ثادق',
+          value: 'Thadiq',
+        },
+        {
+          name: 'تمير',
+          value: 'Tumair',
+        },
+        {
+          name: 'وادي الدواسر',
+          value: 'Wadi adDawasir',
+        },
+        {
+          name: 'الزلفي',
+          value: 'Az Zulfi',
+        },
+      ],
+    },
+    {
+      name: 'الشرقية',
+      value: 'Eastern Province',
+      children: [
+        {
+          name: '（المدينة）إختر',
+          value: '',
+        },
+        {
+          name: 'بقيق',
+          value: 'Buqayq',
+        },
+        {
+          name: 'عين دار',
+          value: 'New Ain Dar',
+        },
+        {
+          name: 'الأحساء',
+          value: 'Hassa',
+        },
+        {
+          name: 'عنك',
+          value: 'Anak',
+        },
+        {
+          name: 'بطحاء',
+          value: 'Al Batha',
+        },
+        {
+          name: 'الدمام',
+          value: 'Dammam',
+        },
+        {
+          name: 'الظهران',
+          value: 'Dhahran',
+        },
+        {
+          name: 'حفر الباطن',
+          value: 'Hafar Al Batin',
+        },
+        {
+          name: 'حرض',
+          value: 'Haradh',
+        },
+        {
+          name: 'الحوية',
+          value: 'Hawiyah',
+        },
+        {
+          name: 'الهفوف',
+          value: 'Hofuf',
+        },
+        {
+          name: 'الجبيل',
+          value: 'Jubail',
+        },
+        {
+          name: 'الخفجي',
+          value: 'Khafji',
+        },
+        {
+          name: 'الخبر',
+          value: 'Khobar',
+        },
+        {
+          name: 'المبرز',
+          value: 'Al Mubarraz',
+        },
+        {
+          name: 'مليجة',
+          value: 'Mulayjah',
+        },
+        {
+          name: 'النعيرية',
+          value: 'Nairyah',
+        },
+        {
+          name: 'العثمانية',
+          value: 'Uthmaniyah',
+        },
+        {
+          name: 'القارة',
+          value: 'Al Qarah',
+        },
+        {
+          name: 'قرية العليا',
+          value: 'Qaryat Al Ulya',
+        },
+        {
+          name: 'القطيف',
+          value: 'Qatif',
+        },
+        {
+          name: 'القيصومة',
+          value: 'Al Qaisumah',
+        },
+        {
+          name: 'رأس الخير',
+          value: 'Ras Al Khair',
+        },
+        {
+          name: 'رأس تنورة',
+          value: 'Ras Tanura',
+        },
+        {
+          name: 'السفانية',
+          value: 'As Saffaniyah',
+        },
+        {
+          name: 'صفوى',
+          value: 'Safwa',
+        },
+        {
+          name: 'سلوى',
+          value: 'Salwa',
+        },
+        {
+          name: 'الصرّار',
+          value: 'As Sarrar',
+        },
+        {
+          name: 'سيهات',
+          value: 'Saihat',
+        },
+        {
+          name: 'تاروت',
+          value: 'Tarout',
+        },
+        {
+          name: 'العضيلية',
+          value: 'Udhailiyah',
+        },
+        {
+          name: 'العيون',
+          value: 'Al Uyun',
+        },
+      ],
+    },
+    {
+      name: 'حائل',
+      value: 'Hail Province',
+      children: [
+        {
+          name: '（المدينة）إختر',
+          value: '',
+        },
+        {
+          name: 'الحائط',
+          value: 'Al Hait',
+        },
+        {
+          name: 'الشملي',
+          value: 'Ash Shamli',
+        },
+        {
+          name: 'بقعاء',
+          value: 'Baqaa',
+        },
+        {
+          name: 'حائل',
+          value: 'Hail',
+        },
+      ],
+    },
+    {
+      name: 'مكة المكرمة',
+      value: 'Makkah Province',
+      children: [
+        {
+          name: '（المدينة）إختر',
+          value: '',
+        },
+        {
+          name: 'الهدا',
+          value: 'Alhada',
+        },
+        {
+          name: 'عمق',
+          value: 'Amaq',
+        },
+        {
+          name: 'عسفان',
+          value: 'Asfan',
+        },
+        {
+          name: 'بحره',
+          value: 'Bahrah',
+        },
+        {
+          name: 'البرك',
+          value: 'Al Birk',
+        },
+        {
+          name: 'ذهبان',
+          value: 'Dahaban',
+        },
+        {
+          name: 'جعرانة',
+          value: "Al Ju'ranah",
+        },
+        {
+          name: 'جدة',
+          value: 'Jeddah',
+        },
+        {
+          name: 'الجموم',
+          value: 'Al Jumum',
+        },
+        {
+          name: 'خليص',
+          value: 'Khulais',
+        },
+        {
+          name: 'الخرمة',
+          value: 'Al Khurma',
+        },
+        {
+          name: 'الليث',
+          value: 'Al Lith',
+        },
+        {
+          name: 'مكه',
+          value: 'Makkah',
+        },
+        {
+          name: 'مستورة',
+          value: 'Mastorah',
+        },
+        {
+          name: 'نمران',
+          value: 'Nimran',
+        },
+        {
+          name: 'القنفذة',
+          value: 'Al Qunfudhah',
+        },
+        {
+          name: 'رابغ',
+          value: 'Rabigh',
+        },
+        {
+          name: 'رنية',
+          value: 'Ranyah',
+        },
+        {
+          name: 'الطائف',
+          value: 'Taif',
+        },
+        {
+          name: 'ثول',
+          value: 'Thuwal',
+        },
+        {
+          name: 'تربة',
+          value: 'Turbah',
+        },
+        {
+          name: 'النوارية',
+          value: 'An Nawwariyyah',
+        },
+      ],
+    },
+    {
+      name: 'القصيم',
+      value: 'Al Qassim Province',
+      children: [
+        {
+          name: '（المدينة）إختر',
+          value: '',
+        },
+        {
+          name: 'الدليمية',
+          value: 'Al Dulaymiyah',
+        },
+        {
+          name: 'الرس',
+          value: 'Ar Rass',
+        },
+        {
+          name: 'عين ابن فهيد',
+          value: 'Ayn Ibn Fuhayd',
+        },
+        {
+          name: 'البدائع',
+          value: 'Al Badayea',
+        },
+        {
+          name: 'البكيرية',
+          value: 'Al Bukayriyah',
+        },
+        {
+          name: 'بريدة',
+          value: 'Buraydah',
+        },
+        {
+          name: 'ضرية',
+          value: 'Dariyah',
+        },
+        {
+          name: 'مذنب',
+          value: 'Al Mithnab',
+        },
+        {
+          name: 'عنيزة',
+          value: 'Unayzah',
+        },
+        {
+          name: 'رياض الخبراء',
+          value: 'Riyadh Al Khabra',
+        },
+        {
+          name: 'التنومه',
+          value: 'Tanumah',
+        },
+        {
+          name: 'عقلة الصقور',
+          value: 'Uglat Asugour',
+        },
+      ],
+    },
+    {
+      name: 'الجوف',
+      value: 'Al Jouf Province',
+      children: [
+        {
+          name: '（المدينة）إختر',
+          value: '',
+        },
+        {
+          name: 'أبو عجرم',
+          value: 'Abu Ajram',
+        },
+        {
+          name: 'الجوف',
+          value: 'Al Jouf',
+        },
+        {
+          name: 'دومة الجندل',
+          value: 'Dumah Al Jandal',
+        },
+        {
+          name: 'الحديثة',
+          value: 'Al Hadithah',
+        },
+        {
+          name: 'حالة عمار',
+          value: 'Halat Ammar',
+        },
+        {
+          name: 'القريات',
+          value: 'Al Qurayyat',
+        },
+        {
+          name: 'سكاكا',
+          value: 'Sakaka',
+        },
+        {
+          name: 'طبرجل',
+          value: 'Tabarjal',
+        },
+      ],
+    },
+    {
+      name: 'عسير',
+      value: 'Asir Province',
+      children: [
+        {
+          name: '（المدينة）إختر',
+          value: '',
+        },
+        {
+          name: 'ابها',
+          value: 'Abha',
+        },
+        {
+          name: 'أحد رفيدة',
+          value: 'Ahad Rafidah',
+        },
+        {
+          name: 'بللسمر',
+          value: 'Billasmar',
+        },
+        {
+          name: 'بارق',
+          value: 'Bariq',
+        },
+        {
+          name: 'بيشة',
+          value: 'Bisha',
+        },
+        {
+          name: 'ظهران الجنوب',
+          value: 'Dhahran Al Janub',
+        },
+        {
+          name: 'الحرجة',
+          value: 'Harajah',
+        },
+        {
+          name: 'خميس مشيط',
+          value: 'Khamis Mushait',
+        },
+        {
+          name: 'المجاردة',
+          value: 'Almajaridah',
+        },
+        {
+          name: 'محايل عسير',
+          value: 'Muhayil',
+        },
+        {
+          name: 'النماص',
+          value: 'Al Namas',
+        },
+        {
+          name: 'رجال ألمع',
+          value: 'Ragal Almaa',
+        },
+        {
+          name: 'سبت العلايا',
+          value: 'Sabt Al Alayah',
+        },
+        {
+          name: 'سراة عبيدة',
+          value: 'Sarat Abidah',
+        },
+        {
+          name: 'تنومة',
+          value: 'Tanomah',
+        },
+        {
+          name: 'تثليث',
+          value: 'Tathleeth',
+        },
+        {
+          name: 'الواديين',
+          value: 'Al Wadeen',
+        },
+        {
+          name: 'وادي ابن هشبل',
+          value: 'Wadi Ibn Hashbal',
+        },
+      ],
+    },
+    {
+      name: 'جازان',
+      value: 'Jizan Province',
+      children: [
+        {
+          name: '（المدينة）إختر',
+          value: '',
+        },
+        {
+          name: 'أبو عريش',
+          value: 'Abu Arish',
+        },
+        {
+          name: 'الداير',
+          value: 'Addayer',
+        },
+        {
+          name: 'أحد المسارحة',
+          value: 'Ahad Al Masarihah',
+        },
+        {
+          name: 'العارضة',
+          value: 'Al Aridhah',
+        },
+        {
+          name: 'بيش',
+          value: 'Baish',
+        },
+        {
+          name: 'ضمد',
+          value: 'Damad',
+        },
+        {
+          name: 'الدرب',
+          value: 'Ad Darb',
+        },
+        {
+          name: 'فرسان',
+          value: 'Farasan',
+        },
+        {
+          name: 'جيزان',
+          value: 'Jizan',
+        },
+        {
+          name: 'الكربوس',
+          value: 'Al Karbus',
+        },
+        {
+          name: 'صبيا',
+          value: 'Sabya',
+        },
+        {
+          name: 'صامطة',
+          value: 'Samtah',
+        },
+      ],
+    },
+    {
+      name: 'نجران',
+      value: 'Najran Province',
+      children: [
+        {
+          name: '（المدينة）إختر',
+          value: '',
+        },
+        {
+          name: 'نجران',
+          value: 'Najran',
+        },
+        {
+          name: 'شرورة',
+          value: 'Sharorah',
+        },
+        {
+          name: 'حبونا',
+          value: 'Hubuna',
+        },
+        {
+          name: 'ثار',
+          value: 'Thar',
+        },
+        {
+          name: 'يدمة',
+          value: 'Yadamah',
+        },
+      ],
+    },
+    {
+      name: 'الحدود الشمالية',
+      value: 'Northern Borders Province',
+      children: [
+        {
+          name: '（المدينة）إختر',
+          value: '',
+        },
+        {
+          name: 'عرعر',
+          value: 'Arar',
+        },
+        {
+          name: 'رفحاء',
+          value: 'Rafha',
+        },
+        {
+          name: 'طريف',
+          value: 'Turaif',
+        },
+      ],
+    },
+    {
+      name: 'تبوك',
+      value: 'Tabuk Province',
+      children: [
+        {
+          name: '（المدينة）إختر',
+          value: '',
+        },
+        {
+          name: 'البدع',
+          value: 'Al Bad',
+        },
+        {
+          name: 'ضبا',
+          value: 'Duba',
+        },
+        {
+          name: 'حقل',
+          value: 'Haql',
+        },
+        {
+          name: 'تبوك',
+          value: 'Tabuk',
+        },
+        {
+          name: 'تيماء',
+          value: 'Tayma',
+        },
+        {
+          name: 'أملج',
+          value: 'Umluj',
+        },
+        {
+          name: 'الوجه',
+          value: 'Al Wajh',
+        },
+      ],
+    },
+  ]
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
   const [whatsapp, setWhatsapp] = useState('');
